efc2fdbf8c2c31a3832eca043611df8b9dbbbdf9
ahmed nader <algammal@eg.ibm.com>
2022-02-08 20:44:50 +0200

    enhanced UX

--- src/components/Addfile/Addfile.js
+++ src/components/Addfile/Addfile.js
@@ -3,13 +3,18 @@ import './Addfile.css'
 const Addfile = (props) => {
     return (
         <div className="buttonContainer">
-            <label htmlFor="csv">Import "CSV" file</label>
-            <input type="file"
-                id="csv" name="csv"
-                accept="CSV, XLSX"
-                onChange={props.csvFile}
-            >
-            </input>
+            <div>
+                <label htmlFor="csv">Import "CSV" file</label>
+                <input type="file"
+                    id="csv" name="csv"
+                    accept="CSV, XLSX"
+                    onChange={props.csvFile}
+                >
+                </input>
+            </div>
+            <div className='note'>
+                Note: Row for pairs worked as a team longest is highlighted in blue
+            </div>
         </div>
     )
 }
